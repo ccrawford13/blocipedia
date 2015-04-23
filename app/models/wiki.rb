9383d1e88faf2636a0ff505365e669728baa6e4f
Chad Crawford <ccrawforduwlax@gmail.com>
2015-04-22 21:31:02 -0500

    Add Wiki length, & presence validations

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -3,6 +3,10 @@ class Wiki < ActiveRecord::Base
   has_many :collaborations
   has_many :collaborators, through: :collaborations, source: :user
 
+  validates :user, presence: true
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+
   scope :private_wikis, -> (user) { where(private: true) }
   scope :public_wikis, -> (user) { where(private: false) }
 
