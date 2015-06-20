61e7fd2a11e464dd9cfc3ff8b87c9f263157f29e
Chad Crawford <ccrawforduwlax@gmail.com>
2015-06-19 21:29:00 -0500

    Redirect all unauthorized requests to sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from Pundit::NotAuthorizedError do |exception|
-     redirect_to root_url, alert: exception.message
-   end
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  def user_not_authorized
+    flash[:notice] = "You must be signed in to perform that action"
+    redirect_to new_user_session_path
   end
 end
